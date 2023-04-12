6f0d28d9fce908e8fbc9c50b70049e21facec5f0
gitdeepaks <deepaksankhyan92@outlook.com>
2023-04-12 17:26:25 +0530

    adding styles

--- src/components/NavBar/NavBar.jsx
+++ src/components/NavBar/NavBar.jsx
@@ -1,10 +1,35 @@
-import React from 'react'
+import React from 'react';
+import { AppBar, IconButton, Toolbar, Button, Avatar, useMediaQuery } from '@mui/material';
+import { Menu, AccountCircle, Brightness4, Brightness7 } from '@mui/icons-material';
+import { Link } from 'react-router-dom';
+import useStyles from './styles'
 
 const NavBar = () => {
-    console.log('NavBar')
+    const classes = useStyles();
+    const isMobile = useMediaQuery('(max-width:600px)');
+
     return (
-        <div>NavBar</div>
+        <>
+            <AppBar position='fixed'>
+                <Toolbar className={classes.toolbar}>
+                    {isMobile && (
+                        <IconButton
+                            color="inherit"
+                            edge="start"
+                            style={{ outline: 'none' }}
+                            onClick={() => { }}
+                            className={classes.menuButton}
+
+                        >
+                            <Menu />
+                        </IconButton>
+                    )}
+
+                </Toolbar>
+
+            </AppBar>
+        </>
     )
 }
 
-export default NavBar
+export default NavBar;
